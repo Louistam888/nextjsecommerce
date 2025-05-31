cc730c192a0df15242b5b40b234554c58a4fccc6
Louistam888 <louistam888@gmail.com>
2025-05-31 18:17:34 -0400

    added some styling to navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="flex min-h-full flex-col bg-white">
         <NavBar />
-        <main>{children}</main>
+        <main className="flex-grow container mx-auto px-4 py-8">
+          {children}
+        </main>
       </body>
     </html>
   );
 }
+
+//12:00
