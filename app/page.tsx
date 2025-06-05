eabf368d7a1e7c55627bec1c741e6dce1f289466
Louistam888 <louistam888@gmail.com>
2025-06-04 20:30:59 -0400

    added css

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { Carousel } from "@/components/ui/carousel";
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -12,13 +13,26 @@ export default async function Home() {
 
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to Murica</h2>
-            <p>Discover the latest products made in the USA</p>
-            <Button asChild variant="default">
-              <Link href="/products">Browse All Products</Link>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md:text-4xl">
+              Welcome to Murica
+            </h2>
+            <p className="text-neutral-600">
+              Discover the latest products made in the USA
+            </p>
+            <Button
+              asChild
+              variant="default"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+            >
+              <Link
+                href="/products"
+                className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >
+                Browse All Products
+              </Link>
             </Button>
           </div>
           <Image
