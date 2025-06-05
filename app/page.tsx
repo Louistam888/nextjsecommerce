fede6c378bbf18b404aca701a19e5ba3873e35a3
Louistam888 <louistam888@gmail.com>
2025-06-04 20:22:17 -0400

    fetched products from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,34 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { stripe } from "@/lib/stripe";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return <div></div>;
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 5,
+  });
+
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2>Welcome to Murica</h2>
+            <p>Discover the latest products made in the USA</p>
+            <Button asChild variant="default">
+              <Link href="/products">Browse All Products</Link>
+            </Button>
+          </div>
+          <Image
+            alt="Banner Image"
+            width={450}
+            height={450}
+            src={products.data[0].images[0]}
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
